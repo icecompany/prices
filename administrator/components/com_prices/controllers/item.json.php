a3d9d9baa3b1391bc8b587953f2c7d533ffa1525
xakepok@xakepok.com <44duP_yx579>
2020-05-14 22:06:24 +0300

    Item json controller

---
+++ administrator/components/com_prices/controllers/item.json.php
@@ -0,0 +1,17 @@
+<?php
+defined('_JEXEC') or die;
+use Joomla\CMS\Response\JsonResponse;
+use Joomla\CMS\MVC\Controller\FormController;
+
+class PricesControllerItem extends FormController {
+    public function getModel($name = 'Item', $prefix = 'PricesModel', $config = array())
+    {
+        return parent::getModel($name, $prefix, $config);
+    }
+
+    public function execute($task)
+    {
+        $item = $this->getModel()->getItem();
+        echo new JsonResponse($item);
+    }
+}
