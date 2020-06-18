6f55d645ea521908f417b60976e443d2c5c1ba59
xakepok@xakepok.com <44duP_yx579>
2020-06-18 14:00:51 +0300

    ProjectID from mkv settings

--- administrator/components/com_prices/models/fields/priceitem.php
+++ administrator/components/com_prices/models/fields/priceitem.php
@@ -25,8 +25,7 @@ class JFormFieldPriceItem extends JFormFieldGroupedList
 
         JTable::addIncludePath(JPATH_ADMINISTRATOR . "/components/com_prj/tables");
         $table = JTable::getInstance('Projects', 'TablePrj');
-        $project = PrjHelper::getActiveProject();
-        if (!is_numeric($project)) $project = 11;
+        $project = PrjHelper::getActiveProject() ?? MkvHelper::getConfig('default_project');
         if (is_numeric($project)) {
             $table->load($project);
             if (is_numeric($table->priceID)) {
