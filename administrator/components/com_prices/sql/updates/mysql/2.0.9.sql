04bb88e6c6b56396388bc67f8a2e0c1118ecef17
asharikov <asharikov@icecompany.org>
2021-01-13 16:57:37 +0300

    Transferred item type

---
+++ administrator/components/com_prices/sql/updates/mysql/2.0.9.sql
@@ -0,0 +1,2 @@
+alter table `#__mkv_price_items` modify `type` set('square', 'electric', 'internet', 'multimedia', 'water', 'cleaning', 'badge', 'welcome', 'fine', 'transfer', 'reg', 'technical', 'transferred') null;
+
