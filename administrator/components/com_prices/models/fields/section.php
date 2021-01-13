5a829d9e7c876a1129fd85ffb789c527e56369c7
asharikov <asharikov@icecompany.org>
2021-01-13 16:44:50 +0300

    Filter in section field

--- administrator/components/com_prices/models/fields/section.php
+++ administrator/components/com_prices/models/fields/section.php
@@ -10,7 +10,7 @@ class JFormFieldSection extends JFormFieldGroupedList
 
     protected function getGroups()
     {
-        $db =& JFactory::getDbo();
+        $db = JFactory::getDbo();
         $query = $db->getQuery(true);
         $query
             ->select("s.id, s.title as section")
@@ -18,6 +18,33 @@ class JFormFieldSection extends JFormFieldGroupedList
             ->from("`#__mkv_price_sections` s")
             ->leftJoin("`#__mkv_prices` p on p.id = s.priceID")
             ->order("s.id desc");
+
+        $input = JFactory::getApplication()->input;
+        $id = $input->getInt('id', 0);
+
+        if ($input->getString('option') == 'com_prices' && $input->getString('view') == 'item') {
+            if ($id === 0) {
+                $project = PrjHelper::getActiveProject(null) ?? MkvHelper::getConfig('default_project');
+                if (is_numeric($project)) {
+                    JTable::addIncludePath(JPATH_ADMINISTRATOR . "/components/com_prj/tables");
+                    $table = JTable::getInstance('Projects', 'TablePrj');
+                    $table->load($project);
+                    $priceID = $table->priceID;
+                }
+            }
+            else {
+                $table = JTable::getInstance('Items', 'TablePrices');
+                $table->load($id);
+                $sectionID = $table->sectionID;
+                if (is_numeric($sectionID)) {
+                    $table = JTable::getInstance('Sections', 'TablePrices');
+                    $table->load($sectionID);
+                    $priceID = $table->priceID;
+                }
+            }
+            if (is_numeric($priceID)) $query->where("s.priceID = {$priceID}");
+        }
+
         $result = $db->setQuery($query)->loadObjectList();
 
         $options = [];
